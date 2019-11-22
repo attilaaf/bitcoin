45f1654d8dc150d88c41ed8ae82664b0b6aa3c23
Attila <bitcoinfiles@protonmail.com>
2019-11-22 17:37:42 +0700

    fix conflict

--- src/txmempool.h
+++ src/txmempool.h
@@ -580,10 +580,10 @@ private:
     typedef std::map<txiter, TxLinks, CompareIteratorByHash> txlinksMap;
     txlinksMap mapLinks;
 
- 
+
     void updateParentNL(txiter entry, txiter parent, bool add);
     void updateChildNL(txiter entry, txiter child, bool add);
- 
+
     typedef std::map<CMempoolAddressDeltaKey, CMempoolAddressDelta, CMempoolAddressDeltaKeyCompare> addressDeltaMap;
     addressDeltaMap mapAddress;
 
@@ -637,7 +637,6 @@ public:
     // to track size/count of descendant transactions. First version of
     // AddUnchecked can be used to have it call CalculateMemPoolAncestors(), and
     // then invoke the second version.
-<<<<<<< HEAD
     void AddUnchecked(
             const uint256 &hash,
             const CTxMemPoolEntry &entry,
@@ -666,8 +665,6 @@ public:
             size_t* pnMempoolSize = nullptr,
             size_t* pnDynamicMemoryUsage = nullptr);
 
-    void RemoveRecursive(
-=======
     bool addUnchecked(const uint256 &hash, const CTxMemPoolEntry &entry,
                       bool validFeeEstimate = true);
     bool addUnchecked(const uint256 &hash, const CTxMemPoolEntry &entry,
@@ -693,8 +690,7 @@ public:
     bool getSpentIndex(CSpentIndexKey &key, CSpentIndexValue &value);
     bool removeSpentIndex(const uint256 txhash);
 
-    void removeRecursive(
->>>>>>> Full address index
+    void RemoveRecursive(
         const CTransaction &tx,
         const mining::CJournalChangeSetPtr& changeSet,
         MemPoolRemovalReason reason = MemPoolRemovalReason::UNKNOWN);
